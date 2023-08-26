df87922a019fa4208129b14727397d450a509df8
nobin-puffles <nobin@puffles.io>
2023-08-26 22:16:43 +0530

    implemented routes for multiple upload

--- routes/delete.routes.js
+++ routes/delete.routes.js
@@ -3,4 +3,5 @@ const router=express.Router();
 const deleteController=require('../controllers/delete.controller');
 const passport=require('passport')
 router.post('/nft',passport.authenticate('jwt',{session:false}),deleteController.prototype.removeNft);
+router.post('/placeholder',passport.authenticate('jwt',{session:false}),deleteController.prototype.removePlaceHolderImage)
 module.exports=router;
--- routes/nft.routes.js
+++ routes/nft.routes.js
@@ -5,5 +5,7 @@ const storage = multer.memoryStorage(); // Store files in memory
 const upload = multer({ storage });
 module.exports=(app)=>{
     app.post('/upload_v1',passport.authenticate('jwt',{session:false}),upload.single("file"),NFTController.upload_v1),
+    app.post('/title',passport.authenticate('jwt',{session:false}),NFTController.title),
+    app.post('/placeholder',passport.authenticate('jwt',{session:false}),NFTController.placeholder_image)
     app.get('/get_nfts',passport.authenticate('jwt',{session:false}),NFTController.get_nfts)
 }
