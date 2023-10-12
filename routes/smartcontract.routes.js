d7e538756856b7194af78da6dac58bf6178e9595
nobin-puffles <nobin@puffles.io>
2023-10-12 19:36:03 +0530

    removed middleware for general routes

--- routes/smartcontract.routes.js
+++ routes/smartcontract.routes.js
@@ -3,5 +3,5 @@ const passport=require("passport")
 module.exports=(app)=>{
     app.post('/smartcontract',passport.authenticate('jwt',{session:false}),SmartContractController.smartcontract)
     app.post('/get_contract',passport.authenticate('jwt',{session:false}),SmartContractController.get_contract)
-    app.post('/address_by_title',passport.authenticate('jwt',{session:false}),SmartContractController.get_address)
+    app.post('/address_by_title',SmartContractController.get_address)
 }
