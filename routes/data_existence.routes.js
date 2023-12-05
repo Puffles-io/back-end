b66070e290160f033bb3654c8a4330757a05641a
nobin-puffles <nobin@puffles.io>
2023-12-05 21:57:07 +0530

    Updated date existence name API

--- routes/data_existence.routes.js
+++ routes/data_existence.routes.js
@@ -1,6 +1,6 @@
 const DataController=require('../controllers/data_existence.controller');
 const express=require('express');
 const router=express.Router();
-router.get('/isCollectionName/:name',DataController.prototype.isCollectionExistByName);
+router.get('/isCollectionURI/:URI',DataController.prototype.isCollectionExistByURI);
 router.get('/isCollectionURL/:urlString',DataController.prototype.isCollectionEXistByURL);
 module.exports=router;
