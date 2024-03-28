befbcf8baab3a95a5fa6e2b09cb5c0c4067e2b6a
nobin-puffles <nobin@puffles.io>
2024-03-28 23:26:22 +0530

    Added jsonobject array response

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 class File {
   async URLAppend(req, res) {
     try {
@@ -7,10 +8,16 @@ class File {
       ) {
         res.status(200).json({ status: false, message: "Missing Data" });
       } else {
+        let jsonFiles = [];
         for (let i = 0; i < req.body.jsonFiles; i++) {
-          req.body.jsonFiles[i].image = req.body.nftUrls[i];
+          let jsonFile = fs.readFileSync(req.body.jsonFiles[i]);
+          let jsonObject = JSON.parse(jsonFile);
+          jsonObject["image"] = req.body.nftUrls[i];
+          jsonFiles.push(jsonObject);
+
+          //req.body.jsonFiles[i].image = req.body.nftUrls[i];
         }
-        res.json(200).json({ status: 200, message: req.body });
+        res.json(200).json({ status: 200, message: jsonFiles });
       }
     } catch (err) {
       console.log(err);
