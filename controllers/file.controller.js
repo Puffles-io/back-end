dcf7918a3d1c348b8a9fa2cda99e39ad4f70aeda
nobin-puffles <nobin@puffles.io>
2024-04-01 22:07:36 +0530

    Fixed json file management

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 class File {
   async URLAppend(req, res) {
     try {
-      console.log(req.body)
       if (
         !!!req.body.hasOwnProperty("jsonFiles") ||
         !!!req.body.hasOwnProperty("nftUrls")
@@ -11,19 +10,26 @@ class File {
       } else {
         let jsonFiles = [];
         for (let i = 0; i < req.body.jsonFiles.length; i++) {
-          let string=Buffer.from(req.body.jsonFiles[i],'base64').toString('utf-8')
-          let jsonObject = JSON.parse(string);
-          jsonObject["image"] = req.body.nftUrls[i];
-          jsonFiles.push(jsonObject);
-
-          //req.body.jsonFiles[i].image = req.body.nftUrls[i];
+          try {
+            const jsonString = atob(req.body.jsonFiles[i]);
+    
+            // Parse JSON string to JavaScript object
+            const jsonObject = JSON.parse(jsonString);
+            console.log(jsonObject)
+            jsonObject["image"] = req.body.nftUrls[i];
+            jsonFiles.push(jsonObject);
+          } catch (parseErr) {
+            console.log("Error parsing JSON file:", parseErr);
+            // Handle parse error if needed
+          }
         }
-        res.json(200).json({ status: 200, message: jsonFiles });
+        res.status(200).json({ status: 200, message: jsonFiles });
       }
     } catch (err) {
       console.log(err);
       res.status(200).json({ status: false, message: "Server Error" });
     }
   }
+  
 }
 module.exports = File;
