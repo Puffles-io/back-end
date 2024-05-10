a207d62cbfefdba8061b5a5af8b378cdf0980ba4
nobin-puffles <nobin@puffles.io>
2024-05-11 00:01:43 +0530

    added ever_mint_started

--- controllers/update.controller.js
+++ controllers/update.controller.js
@@ -378,7 +378,8 @@ class Update {
           let updateExpression = "";
           let expressionAttributeNames = {};
           let expressionAttributeValues = {};
-          if (!req.body.hasOwnProperty("ever_mint_started")) {
+          console.log(results.Item);
+          if (results.Item.hasOwnProperty("ever_mint_started")) {
             updateExpression = "set #active_phase=:active_phase";
             expressionAttributeNames = { "#active_phase": "active_phase" };
             expressionAttributeValues = {
@@ -393,7 +394,7 @@ class Update {
             };
             expressionAttributeValues = {
               ":active_phase": req.body.active_phase,
-              ":ever_mint_started": req.body.ever_mint_started,
+              ":ever_mint_started": true,
             };
           }
           const updatedParams = {
